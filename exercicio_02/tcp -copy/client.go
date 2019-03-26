4762243c65211ca14d10d69beff8b70c0589aef4
Guilherme Calland Rosa Borba <gcrb@WINDOWS.CIN.UFPE.BR>
2019-03-26 17:19:11 -0300

    mudanças

---
+++ exercicio_02/tcp -copy/client.go
@@ -0,0 +1,47 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+)
+
+
+func main(){
+
+	//SERVER 01
+	connection_01, error_01 := net.Dial("tcp", ":8080")
+	if error_01 != nil {
+		fmt.Println("Erro ao se conectar no servidor_01 !")
+	}
+	
+	//SERVER 02
+	connection_02, error_02 := net.Dial("tcp",":1313")
+	if error_02 != nil{
+		fmt.Println("Erro ao se conectar no servidor_02 !")
+	}
+
+	for{
+		//le uma msg do teclado
+		reader_01 := bufio.NewReader(os.Stdin)
+		fmt.Print("\nDigite uma mensagem ao servidor_01 : ")
+		client_text_01, _ := reader_01.ReadString('\n')
+		//envia uma mensagem para o servidor
+		fmt.Fprintf(connection_01, client_text_01 + "\n")
+		//escuta por uma resposta do servidor
+		server_text_01, _ := bufio.NewReader(connection_01).ReadString('\n')
+		fmt.Print("Mensagem do Servidor_01: " + server_text_01)
+
+		reader_02 := bufio.NewReader(os.Stdin)
+		fmt.Print("\nDigite uma mensagem ao servidor_02 : ")
+		client_text_02, _ := reader_02.ReadString('\n')
+		fmt.Fprintf(connection_02, client_text_02 + "\n")
+		//escuta por uma resposta do servidor
+		server_text_02, _ := bufio.NewReader(connection_02).ReadString('\n')
+		fmt.Print("Mensagem do Servidor_02: " + server_text_02)
+	}
+
+	connection_01.Close()
+	connection_02.Close()
+}
---
+++ exercicio_02/udp - copy/server_01.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+    "net"
+    "fmt"
+)
+
+func main(){
+	server_connection, _ := net.ListenUDP("udp", &net.UDPAddr{IP:[]byte{0,0,0,0},Port:1313,Zone:""})
+	defer server_connection.Close()
+	
+	buf := make([]byte, 1024)
+	n, addr, _ := server_connection.ReadFromUDP(buf)
+	fmt.Println("Mensagem do cliente: ", string(buf[0:n]), "do endereco IP:", addr)
+	
+}
+
+
